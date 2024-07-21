6cb65c04571d6d9a808b7ab538c69ab6233eb369
Anisur Rahman <anisur@wpgigs.com>
2024-07-21 16:23:28 +0600

    Rename var

--- includes/Admin/Menu.php
+++ includes/Admin/Menu.php
@@ -6,10 +6,10 @@ namespace CE\Library_Master\Admin;
  * Menu class
  */
 class Menu {
-	public $addressbook;
+	public $master_library;
 
-	public function __construct( $addressbook ) {
-		$this->addressbook = $addressbook;
+	public function __construct( $master_library ) {
+		$this->master_library = $master_library;
 
 		add_action( 'admin_menu', array( $this, 'admin_menu' ) );
 	}
@@ -19,8 +19,8 @@ class Menu {
 		$parent_slug = 'library-master';
 
 		$page_title = __( 'Library Master', 'library-master' );
-		$hook       = add_menu_page( $page_title, $page_title, $capability, $parent_slug, array( $this->addressbook, 'plugin_page' ), 'dashicons-welcome-learn-more' );
-		add_submenu_page( $parent_slug, __( 'Library Master', 'library-master' ), __( 'Library Master', 'library-master' ), $capability, 'library-master', array( $this->addressbook, 'plugin_page' ) );
+		$hook       = add_menu_page( $page_title, $page_title, $capability, $parent_slug, array( $this->master_library, 'plugin_page' ), 'dashicons-welcome-learn-more' );
+		add_submenu_page( $parent_slug, __( 'Library Master', 'library-master' ), __( 'Library Master', 'library-master' ), $capability, 'library-master', array( $this->master_library, 'plugin_page' ) );
 		add_submenu_page( $parent_slug, __( 'Settings', 'library-master' ), __( 'Settings', 'library-master' ), $capability, 'library-master-settings', array( $this, 'settings_page' ) );
 
 		add_action( 'admin_head-' . $hook, array( $this, 'enqueue_assets' ) );
